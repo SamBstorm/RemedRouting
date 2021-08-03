270762cad3c0e4c8d2a30d2b9de0a18aaff43489
Legrain Samuel <samuel.legrain@bstorm.be>
2021-08-03 17:00:22 +0200

    Add routing basis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { RedPageComponent } from './Components/red-page/red-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BluePageComponent } from './Components/blue-page/blue-page.component';
+import { YellowPageComponent } from './Components/yellow-page/yellow-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"rouge", component: RedPageComponent},
+  {path:"bleu", component: BluePageComponent},
+  {path:"jaune", component: YellowPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedPageComponent } from './Components/red-page/red-page.component';
+import { BluePageComponent } from './Components/blue-page/blue-page.component';
+import { YellowPageComponent } from './Components/yellow-page/yellow-page.component';
+import { NavigationMenuComponent } from './Components/navigation-menu/navigation-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedPageComponent,
+    BluePageComponent,
+    YellowPageComponent,
+    NavigationMenuComponent
   ],
   imports: [
     BrowserModule,
